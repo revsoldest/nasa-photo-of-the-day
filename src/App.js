02ab0f1c84dfcdb695637b5945addbe8bd0ec2ea
Elijah Conley <elijahandaleciael@gmail.com>
2019-10-22 21:12:31 -0400

    got every thing to show up done for days part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import nasaList from "./Component/nasaList";
+import NasaApi from "./Component/nasaList";
 
 function App() {
   return (
     <div className="App">
       <h2> NASA Images of The Day!! </h2>
-      <nasaList />
+      <NasaApi />
     </div>
   );
 }
